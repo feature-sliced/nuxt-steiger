05dcfac66c88d78e302796a33a27e72e9e0160b5
Lev Chelyadinov <leva181777@gmail.com>
2024-10-26 00:04:29 +0200

    Add files

---
+++ pages/home/ui/test.vue
@@ -0,0 +1,11 @@
+<script setup>
+  import { a } from '@/features/session/login';
+  import { b } from '@/features/session/logout';
+  import { c } from '@/features/session/register';
+</script>
+
+<template>
+{{ a }}
+{{ b }}
+{{ c }}
+</template>
